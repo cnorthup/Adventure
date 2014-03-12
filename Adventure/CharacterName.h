9ddac0110b4754535d3f7622c69990aad2e04aa1
Charles Northup <cjnorth40@gmail.com>
2014-03-11 22:34:50 -0500

    changes name

---
+++ Adventure/CharacterName.h
@@ -0,0 +1,13 @@
+//
+//  CharacterName.h
+//  Adventure
+//
+//  Created by Charles Northup on 3/11/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CharacterName : UITextField
+
+@end
